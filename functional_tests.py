83daff40dc8a8c488aa7de6842a7ba9e77607df9
Kirill Sh <kshumov89@gmail.com>
2021-07-30 14:50:29 +0300

    Рефакторинг unittesta

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,12 @@ class NewVisitorTest(unittest.TestCase):
         '''Демонтаж'''
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        '''подтверждение нахождения строки в таблице списка'''
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''тест: можно начать список и получить его позже'''
         # Даша слышала про крутое новое приложение со списком
@@ -42,19 +48,19 @@ class NewVisitorTest(unittest.TestCase):
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows),
-            "Новый элемент списка не появляется в таблице"
-        )
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
-        self.fail('Закончить тест!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # Страница снова обновляется и теперь показывает оба элемента ее списка
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+        self.fail('Закончить тест!')
 
         # Даше интересно, заполнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес - об этом
--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,5 @@
-from django.http import HttpRequest
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page
+
 
 class HomePageTest(TestCase):
     '''Тестирование стартовой страницы'''
@@ -9,6 +7,12 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         '''тест: используется домашний шаблон'''
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        '''тест можно сохранить как post-запрос'''
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_returns_correct_html(self):
