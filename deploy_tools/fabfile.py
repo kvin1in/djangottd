38ce3a15e0d42cb693b6222b6d944a43a673ab0c
Kirill Sh <kshumov89@gmail.com>
2021-08-05 15:56:09 +0300

    Add fab config

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,72 @@
+import random
+
+from fabric.api import env, run, local
+from fabric.contrib.files import exists, sed, append
+
+REPO_URL = 'https://github.com/kvin1in/djangottd.git'
+
+
+def deploy():
+    '''развернуть'''
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    source_folder = site_folder + '/source'
+    _create_dictionary_structure_if_necessary(site_folder)
+    _get__latest_source(source_folder)
+    _update_settings(site_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_static_files(site_folder)
+    _update_database(site_folder)
+
+
+def _create_dictionary_structure_if_necessary(site_folder):
+    '''создание структуры каталога, если это нужно'''
+    for subfolder in ('database', 'static', 'virtualenv', 'source'):
+        run(f'mkdir -p {site_folder}/{subfolder}')
+
+
+def _get__latest_source(source_folder):
+    '''получить свежий исходный код'''
+    if exists(source_folder + '/.git'):
+        run(f'cd {source_folder} && git fetch')
+    else:
+        run(f'git clone {REPO_URL}{source_folder}')
+    current_commit = local('git log -n 1 --format=%H', capture=True)
+    run(f'cd {source_folder} && git reset --hard {current_commit}')
+
+
+def _update_settings(source_folder, site_name):
+    '''обновить настройки'''
+    settings_path = source_folder + '/superlists/settings.py'
+    sed(settings_path, "DEBUG = True", "DEBUG = False")
+    sed(settings_path,
+        'ALLOWED_HOSTS =.+$',
+        f'ALLOWED_HOSTS = ["site_name"]'
+        )
+    secret_key_file = source_folder + '/superlists/settings.py'
+    if not exists(secret_key_file):
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(_-=+)'
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file, f'SECRET_KEY= "{key}"')
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+
+
+def _update_virtualenv(source_folder):
+    '''обновить виртуальную среду'''
+    virtualenv_folder = source_folder + '/../virtualenv'
+    if not exists(virtualenv_folder + 'bin/pip'):
+        run(f'python3.8 -m venv {virtualenv_folder}')
+    run(f'{virtualenv_folder}/bin/pip install -r {source_folder}/requirements.txt')
+
+
+def _update_static_files(source_folder):
+    '''обновить статические файлы'''
+    run(f'cd {source_folder}'
+        ' && ../virtualenv/bin/python manage.py collectstatic --noinput')
+
+
+def _update_database(source_folder):
+    '''обновление базы данных'''
+    run(
+        f'cd {source_folder}'
+        ' && ../virtualenv/bin/python manage.py migrate --noinput'
+    )
