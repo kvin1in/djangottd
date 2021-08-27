7f661ea55fab1c10f3cec2c94805c36c73c00b85
Kirill Sh <kshumov89@gmail.com>
2021-08-27 23:26:25 +0300

    git commit -m 'Перемещены ФТ в свои собственные отдельные файлы'

--- functional_tests/test_cannot_add_empty_list_items.py
+++ functional_tests/test_cannot_add_empty_list_items.py
@@ -1,39 +1,13 @@
-import time
-import os
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+
+from .base import FunctionalTest
 
 MAX_WAIT = 10
 
 
-class NewVisitorTest(StaticLiveServerTestCase):
+class NewVisitorTest(FunctionalTest):
     '''тест нового пользователя'''
-
-    def setUp(self) -> None:
-        self.browser = webdriver.Chrome()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def tearDown(self) -> None:
-        '''Демонтаж'''
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        '''подтверждение нахождения строки в таблице списка'''
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
 
     def test_can_start_a_list_for_one_user(self):
         '''тест: можно начать список и получить его позже'''
@@ -43,8 +17,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел.
-        time.sleep(10)
-
         self.assertIn("To-Do lists", self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -118,29 +90,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Купить павлиньи перья', page_text)
         self.assertIn('Купить молоко', page_text)
-
-    def test_layout_and_styling(self):
-        '''тест макета и стилевого оформления'''
-        # Даша открывает домашнюю страницу
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # Она замечает, что поле аккуратно центрировано
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-        # Она начинает новый список и видит, что поле ввода там тоже
-        # аккуратно центрировано
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
