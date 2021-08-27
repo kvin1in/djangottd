2979b3ffad018e39cb02052ca85cc6208eabe65c
Kirill Sh <kshumov89@gmail.com>
2021-08-28 00:54:57 +0300

    git commit -m 'Модульные тесты разведены в несколько файлов'

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,38 +2,6 @@ from django.test import TestCase
 from lists.models import Item, List
 
 
-class ListAndItemModelTest(TestCase):
-    '''тест модели элемента списка'''
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        '''тест сохранения и получения элементов списка'''
-        first_item = Item()
-        first_item.text = 'This first (even) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'This first (even) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
-
-
 class HomePageTest(TestCase):
     '''Тестирование стартовой страницы'''
 
